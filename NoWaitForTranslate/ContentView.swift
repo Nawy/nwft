0f994efbd09349ced0cc5f1d24b21c5bd983c97b
Ivan Ermolaev <ivan.ermolaev@nl.abnamro.com>
2021-07-31 17:59:27 +0200

    Initial Commit

---
+++ NoWaitForTranslate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoWaitForTranslate
+//
+//  Created by Ivan Ermolaev on 31/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoWaitForTranslate/NoWaitForTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoWaitForTranslateApp.swift
+//  NoWaitForTranslate
+//
+//  Created by Ivan Ermolaev on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NoWaitForTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
